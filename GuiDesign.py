43c279ca4c624aa855b96d202cd15d478f2e3b25
yipinp <yipinp@nvidia.com>
2018-02-04 23:22:23 +0800

    help

--- GuiDesign.py
+++ GuiDesign.py
@@ -6,24 +6,47 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow,self).__init__()
+        self.setWindowTitle("图像处理程序 版本： 0.8")
+        self.resize(640,480)
         self.initUI()
 
     def initUI(self):
-        exitAct = QAction(QIcon('exit.png'),'&Exit',self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit Application')
+        wlayout = QGridLayout()
+
+        hsplitter = QSplitter(Qt.Horizontal)
+        vsplitter = QSplitter(Qt.Vertical)
 
 
-        self.statusBar().showMessage("Ready")
-        menuBar = self.menuBar()
-        fileMenu = menuBar.addMenu('&File')
-        fileMenu.addAction(exitAct)
+        hwg1 = QWidget()
+        hwg2 = QWidget()
+        vwg  = QWidget()
+        hlayout1 = QVBoxLayout()
+        hlayout2 = QVBoxLayout()
+        vlayout = QHBoxLayout()
+
+        hsplitter.addWidget(hwg1)
+        hsplitter.addWidget(hwg2)
+        vsplitter.addWidget(hsplitter)
+        vsplitter.addWidget(vwg)
 
 
-        self.setWindowTitle("version 0.8")
-        self.setGeometry(300,300,300,300)
-        self.setGeometry(300,300,300,300)
-        self.show()
+        hlayout1.addWidget(QPushButton('图像拼接'))
+        hlayout1.addWidget(QPushButton('计算重心'))
+        hlayout1.addWidget(QPushButton('颜色统计图'))
+        hlayout1.addWidget(QPushButton('网格化'))
+
+        hlayout2.addWidget(QPushButton('1'))
+
+        hwg1.setLayout(hlayout1)
+        hwg2.setLayout(hlayout2)
+        vwg.setLayout(vlayout)
+
+
+
+        wlayout.addWidget(vsplitter,0,0)
+        # wlayout.addWidget(hwg2,0,1)
+        # wlayout.addWidget(vwg,1,0)
+        self.setLayout(wlayout)
 
 app = QApplication(sys.argv)
 window = MainWindow()
