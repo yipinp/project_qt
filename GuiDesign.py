75f1691e6661b4c557982dee4e7dff0a0791b0cb
unknown <yipinp@YIPINP-LT1.nvidia.com>
2018-02-05 17:42:16 +0800

    GUI

--- GuiDesign.py
+++ GuiDesign.py
@@ -3,50 +3,78 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import sys
 
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 576
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow,self).__init__()
         self.setWindowTitle("图像处理程序 版本： 0.8")
-        self.resize(640,480)
+        self.resize(WINDOW_WIDTH,WINDOW_HEIGHT)
         self.initUI()
 
     def initUI(self):
-        wlayout = QGridLayout()
+        wwg  = QWidget()
+        hwg1 = QFrame()
+        hwg1.setFrameShape(QFrame.StyledPanel)
+        hwg2 = QFrame()
+        hwg2.setFrameShape(QFrame.StyledPanel)
+        hwg3 = QFrame()
+        hwg3.setFrameShape(QFrame.StyledPanel)
 
+        hbox = QHBoxLayout()
         hsplitter = QSplitter(Qt.Horizontal)
-        vsplitter = QSplitter(Qt.Vertical)
+        vbox = QGridLayout()
+        vbox.setAlignment(Qt.AlignTop)
+        label0 = QLabel('主功能区')
+        label0.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(label0,0,0)
+        butn0 = QPushButton('图像拼接')
+        butn1 = QPushButton('计算重心')
+        butn2 = QPushButton('计算颜色统计图')
+        butn3 = QPushButton('图像网格化')
+        vbox.addWidget(butn0,1,0)
+        vbox.addWidget(butn1,2,0)
+        vbox.addWidget(butn2,3,0)
+        vbox.addWidget(butn3,4,0)
+        hwg1.setLayout(vbox)
 
 
-        hwg1 = QWidget()
-        hwg2 = QWidget()
-        vwg  = QWidget()
-        hlayout1 = QVBoxLayout()
-        hlayout2 = QVBoxLayout()
-        vlayout = QHBoxLayout()
+        vbox1 = QGridLayout()
+        vbox1.setAlignment(Qt.AlignTop)
+        label1 = QLabel('信息显示区')
+        label1.setAlignment(Qt.AlignCenter)
+        vbox1.addWidget(label1,0,0)
+        hwg2.setLayout(vbox1)
 
         hsplitter.addWidget(hwg1)
         hsplitter.addWidget(hwg2)
+        hsplitter.setStretchFactor(0,50)
+        hsplitter.setStretchFactor(1,50)
+        hsplitter.setAutoFillBackground(True)
+
+        vsplitter = QSplitter(Qt.Vertical)
         vsplitter.addWidget(hsplitter)
-        vsplitter.addWidget(vwg)
+        vbox2 = QGridLayout()
+        hwg3.setLayout(vbox2)
+        vbox2.setAlignment(Qt.AlignTop)
+        label2 = QLabel('图片显示区')
+        label2.setAlignment(Qt.AlignCenter)
+        vbox2.addWidget(label2)
+        vsplitter.addWidget(hsplitter)
+        vsplitter.addWidget(hwg3)
+        vsplitter.setStretchFactor(1,4)
+        vsplitter.setStretchFactor(2,6)
+        vsplitter.setAutoFillBackground(True)
 
 
-        hlayout1.addWidget(QPushButton('图像拼接'))
-        hlayout1.addWidget(QPushButton('计算重心'))
-        hlayout1.addWidget(QPushButton('颜色统计图'))
-        hlayout1.addWidget(QPushButton('网格化'))
-
-        hlayout2.addWidget(QPushButton('1'))
-
-        hwg1.setLayout(hlayout1)
-        hwg2.setLayout(hlayout2)
-        vwg.setLayout(vlayout)
+        hbox.addWidget(vsplitter)
+        wwg.setLayout(hbox)
+        self.setCentralWidget(wwg)
 
 
 
-        wlayout.addWidget(vsplitter,0,0)
-        # wlayout.addWidget(hwg2,0,1)
-        # wlayout.addWidget(vwg,1,0)
-        self.setLayout(wlayout)
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
